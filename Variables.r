f960c73943de59a53c7aab6a72de4e7d510b6814
Akshita <31439578+akshita26@users.noreply.github.com>
2019-04-09 21:46:58 +0530

    Add files via upload

---
+++ Variables.r
@@ -0,0 +1,26 @@
+y<-"Hello"
+x="Heyy"
+"Test"->z
+#Variable assignment
+if(FALSE)
+{}
+{}
+NULL
+V<-TRUE
+print(class(V))
+#[1] "logical"
+V<-12.3
+print(class(V))
+#[1] "numeric"
+V<-12L
+print(class(V))
+#[1] "integer"
+V<-12+3i
+print(class(V))
+#[1] "complex"
+V<-"Hi"
+print(class(V))
+#[1] "character"
+V<-charToRaw("abcd")
+print(class(V))
+#[1] "raw"
